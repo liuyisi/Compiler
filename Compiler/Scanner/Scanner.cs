6deaa162be42a6898eb7f844b236d7b681de24dd
liuyisi <121637176@qq.com>
2014-09-23 21:03:28 +0800

    part of DFA
    part of getNextToken without debuging (unfinished)

--- Compiler/Scanner/Scanner.cs
+++ Compiler/Scanner/Scanner.cs
@@ -95,5 +95,152 @@ namespace Compiler.Common
             return character;
         }
 
+
+        /*
+         *author: Louise
+         *date  : 23/09/2014
+         *describe: 识别是当否是数字并且返回这个数字，如果后面跟了字母则报错
+         */
+        public String recoNumber()
+        {
+            String Number = "";
+            while (true)
+            {
+                if (Buffer[Cur] >= '0' && Buffer[Cur] <= '9')
+                {
+                    Number += Buffer[Cur];
+                    Cur++;
+                    Row++;
+                }
+                else break;
+            }
+            if ((Buffer[Cur] >= 'a' && Buffer[Cur] <= 'z') || (Buffer[Cur] >= 'A' && Buffer[Cur] <= 'Z'))
+            {
+                error.Line = Line;
+                error.Row = Row;
+                error.isError = true;
+                error.Type = ErrorType.errorType.LexicalError;
+                return null;
+            }
+            return Number;
+        }
+
+
+        /*
+         *author: Louise
+         *date  : 23/09/2014
+         *describe: 取得由字母开头的标识符或保留字，此处暂时不区分两者，只是切开
+         */
+        public String recoName()
+        {
+            String Name = "";
+            while (true)
+            {
+                if ((Buffer[Cur] >= 'a' && Buffer[Cur] <= 'z')
+                    || (Buffer[Cur] >= 'A' && Buffer[Cur] <= 'Z')
+                    || (Buffer[Cur] >= '0' && Buffer[Cur] <= '9'))
+                {
+                    Name += Buffer[Cur];
+                    Cur++;
+                    Row++;
+                }
+                else break;
+            }
+            return Name;
+        }
+
+        public LexType isReserved(String name)
+        {
+            switch (name)
+            {
+                case "program": 
+                    return LexType.PROGRAM;
+                case "type":
+                    return LexType.TYPE;
+                case "var":
+                    return LexType.VAR;
+                case "procedure":
+                    return LexType.PROCEDURE;
+                case "begin":
+                    return LexType.BEGINI;
+                case "end":
+                    return LexType.ENDI;
+                case "array":
+                    return LexType.ARRAY;
+                case "of":
+                    return LexType.OF;
+                case "record":
+                    return LexType.RECORD;
+                case "if":
+                    return LexType.IF;
+                case "then":
+                    return LexType.THEN;
+                case "else":
+                    return LexType.ELSE;
+                case "fi":
+                    return LexType.FI;
+                case "while":
+                    return LexType.WHILE;
+                case "do":
+                    return LexType.DO;
+                case "endwh":
+                    return LexType.ENDWH;
+                case "read":
+                    return LexType.READ;
+                case "write":
+                    return LexType.WRITE;
+                case "return":
+                    return LexType.RETURN1;
+                case "integer":
+                    return LexType.INTERGER;
+                case "char":
+                    return LexType.CHAR1;
+
+            }
+            return LexType.ID;
+        }
+
+        /*
+         *author: Louise
+         *date  : 23/09/2014
+         *describe: 取得下一个token
+         */
+        public TokenType getNextToken()
+        {
+            char entrance;
+            TokenType Token = new TokenType() ;
+            Token.Line = Line;
+            Token.Row  = Row ;
+            entrance = getNextChar();
+
+            if ((entrance >= 'a' && entrance <='z') || (entrance >= 'A' && entrance <= 'Z'))
+            {
+                Token.Data = recoName();
+                Token.lexType = isReserved(Token.Data);
+                
+            }
+            else if (entrance >= '0' && entrance <= '9')
+            {
+                Token.Data = recoNumber();
+                Token.lexType = LexType.INTC;
+            }
+            else if (entrance == ':' && Buffer[Cur + 1] == '=' )
+            {
+                Cur += 2; Row += 2; 
+                Token.Data = ":=" ;
+                Token.lexType = LexType.ASSIGN ;
+            }
+            else if (entrance == '.' && Buffer[Cur + 1] == '.')
+            {
+                Cur += 2; Row += 2;
+                Token.Data = "..";
+                Token.lexType = LexType.UNDERANGE;
+            }
+            else
+            {
+            }
+
+            return Token;
+        }
     }
 }
